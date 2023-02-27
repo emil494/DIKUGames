ccea4b36fa07467b43412fc8d98cb906104ae706
NikolajSkole <ltf688@alumni.ku.dk>
2023-02-27 14:18:14 +0100

    Test

--- Galaga/Program.cs
+++ Galaga/Program.cs
@@ -7,10 +7,10 @@ namespace Galaga
     {
         static void Main(string[] args)
         {
-            //var windowArgs = new WindowArgs() { Title = "Galaga v0.1" };
-            //var game = new Game(windowArgs);
-            //game.Run();
-            Console.WriteLine("Hello World!"); // TODO: Delete this line!
+            var windowArgs = new WindowArgs() { Title = "Galaga v0.1" };
+            var game = new Game(windowArgs);
+            game.Run();
+            //Console.WriteLine("Hello World!"); // TODO: Delete this line!
         }
     }
 }
