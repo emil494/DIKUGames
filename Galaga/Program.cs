8d07ab4356b1554a0af2ed973d2a6d4a6071b27d
NikolajK1405 <Nikolajkrarup@hotmail.com>
2023-02-27 22:19:49 +0100

    lol

--- Galaga/Program.cs
+++ Galaga/Program.cs
@@ -5,13 +5,13 @@ namespace Galaga
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             var windowArgs = new WindowArgs() { Title = "Galaga v0.1" };
             var game = new Game(windowArgs);
             game.Run();
             //Console.WriteLine("Hello World!"); // TODO: Delete this line!
-            // Hej!
+            // Hej!!
         }
     }
 }
