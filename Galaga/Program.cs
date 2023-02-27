36219bc6b672b9f6da06d24fa9dbb8902d8db944
NikolajK1405 <Nikolajkrarup@hotmail.com>
2023-02-28 00:18:34 +0100

    .

--- Galaga/Program.cs
+++ Galaga/Program.cs
@@ -1,11 +1,11 @@
-using System;
+﻿using System;
 using DIKUArcade.GUI;
 
 namespace Galaga
 {
     class Program
     {
-        static void Main(string[] args) //hej
+        static void Main(string[] args) //hej 
         {
             var windowArgs = new WindowArgs() { Title = "Galaga v0.1" };
             var game = new Game(windowArgs);
