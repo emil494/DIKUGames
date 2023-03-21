5c90b32548109ab2fae3051d29bfd53cd5fb69f9
simonsionsom <simon19653@gmail.com>
2023-03-21 15:54:20 +0100

    Første udkast til TestPlaver TestMoveUp()

---
+++ galagaTests/TestPlayer.cs
@@ -0,0 +1,38 @@
+namespace galagaTests;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.Events;
+using Galaga;
+public class Tests
+{
+    [SetUp]
+   
+    
+    public void Setup()
+    {
+        
+        player = new Player(
+            new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+            new Image(Path.Combine("Assets", "Images", "Player.png")));
+        eventBus = new GameEventBus();
+        eventBus.InitializeEventBus(new List<GameEventType> {GameEventType.PlayerEvent});
+        eventBus.Subscribe(GameEventType.PlayerEvent, player);
+    }
+    private Player player;
+    private GameEventBus eventBus;
+
+    [Test]
+    public void TestMoveUp()
+    {
+        var start = player.GetPosition();
+        eventBus.RegisterEvent(
+            new GameEvent {EventType = GameEventType.PlayerEvent, Message = "MOVE",
+            StringArg1 = "UP"}
+        );
+        player.Move();
+        var temp = player.GetPosition();
+        var OtherTemp = start + 0.01f; 
+        Assert.AreEqual(temp,OtherTemp);
+    }
+}
