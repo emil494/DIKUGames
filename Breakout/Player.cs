8a13b0e55d7ef7264b7c52cd5726289863a2908c
simonsionsom <simon19653@gmail.com>
2023-04-25 17:55:05 +0200

    Player

---
+++ Breakout/Player.cs
@@ -0,0 +1,43 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.Events;
+
+namespace Breakout;
+
+public class Player : Entity {
+/*
+    float moveLeft = 0.0f;
+
+    float moveRight = 0.0f;
+
+    float MOVEMENT_SPEED = 0.01f;
+*/
+    
+    //private DynamicShape shape;
+    public Player(DynamicShape shape, IBaseImage image) : base(shape, image) {
+        //this.image = image;
+        //this.shape = shape;
+    }/*
+private void SetMoveLeft(bool val){
+    if (val){
+        moveLeft = -MOVEMENT_SPEED;
+        UpdateDirection();
+    } else {
+        moveLeft = 0.0f;
+        UpdateDirection();
+    }
+}
+
+private void SetMoveRight(bool val){
+    if (val){
+        moveRight = MOVEMENT_SPEED;
+        UpdateDirection();
+    } else {
+        moveRight = 0.0f;
+        UpdateDirection();
+    }
+*/
+    
+    
+}
