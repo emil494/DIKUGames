7defc9c98205ae65cc5e925a1dfd3a85a9bbe9f4
NikolajK1405 <nikolajkrarup@hotmail.com>
2023-04-27 15:16:37 +0000

    FileReaderTest done

--- BreakoutTests/LevelLoadingTests/FileReaderTest.cs
+++ BreakoutTests/LevelLoadingTests/FileReaderTest.cs
@@ -5,21 +5,58 @@ namespace BreakoutTests;
 public class FileReaderTest
 {
     [SetUp]
+    FileReader reader;
+    string lvl1Path;
     public void Setup() {
         reader = new FileReader();
+        lvl1Path = Path.Combine("..", "..", "..", "Assets", "Levels", "level1.txt");
     }
-
-    FileReader reader;
 
     [Test]
     public void TestReturnTrue() {
-        bool result = reader.Read("level1.txt");
+        bool result = reader.Read(lvl1Path);
         Assert.True(result);
     }
 
     [Test]
-    public void TestReturnFalse() {
-        bool result = reader.Read("levelNoMap.txt");
+    public void TestReturnFalseMap() {
+        bool result = reader.Read(Path.Combine("..", "..", "..", "Assets", "Levels", "levelNoMap.txt"));
         Assert.False(result);
+    }
+
+    [Test]
+    public void TestReturnFalseMeta() {
+        bool result = reader.Read(Path.Combine("..", "..", "..", "Assets", "Levels", "levelNoMeta.txt"));
+        Assert.False(result);
+    }
+
+    [Test]
+    public void TestReturnFalseLegend() {
+        bool result = reader.Read(Path.Combine("..", "..", "..", "Assets", "Levels", "levelNoLegend.txt"));
+        Assert.False(result);
+    }
+
+    [Test]
+    public void TestMapField() {
+        reader.Read(lvl1Path);
+        string estResult = "-aaaaaaaaaa-";
+        string result = reader.map[2];
+        Assert.AreEqual(estResult, result);
+    }
+
+    [Test]
+    public void TestMetaField() {
+        reader.Read(lvl1Path);
+        string estResult = "300";
+        string result = reader.meta["Time"];
+        Assert.AreEqual(estResult, result);
+    }
+
+    [Test]
+    public void TestLegendField() {
+        reader.Read(lvl1Path);
+        string estResult = "orange-block.png";
+        string result = reader.legend['1'];
+        Assert.AreEqual(estResult, result);
     }
 }
