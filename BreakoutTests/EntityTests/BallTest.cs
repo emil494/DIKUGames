9fe304045a1bd67a5683a9c4e76ded4237ddd74b
NikolajK1405 <nikolajkrarup@hotmail.com>
2023-05-12 08:58:46 +0000

    BallTests more progress

--- BreakoutTests/EntityTests/BallTest.cs
+++ BreakoutTests/EntityTests/BallTest.cs
@@ -12,23 +12,26 @@ public class BallTest{
     public void Setup(){
         Window.CreateOpenGLContext();
         player = new Player(
-            new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.2f, 0.03f)),
+            new DynamicShape(new Vec2F(0.5f, 0.46f), new Vec2F(0.2f, 0.03f)),
             new Image(Path.Combine("Assets", "Images", "player.png")));
         
         ball = new Ball(
-            new DynamicShape(new Vec2F(0.44f, 0.17f), new Vec2F(0.06f, 0.06f)),
+            new DynamicShape(new Vec2F(0.5f, 0.5f), new Vec2F(0.05f, 0.05f)),
             new Image(Path.Combine("Assets", "Images", "ball.png")));
 
         block = new Block (
             new DynamicShape(
-                new Vec2F(0.0f, 0.0f), new Vec2F(1/12.0f, 1/25.0f)), 
+                new Vec2F(0.56f, 0.5f), new Vec2F(0.05f, 0.05f)), 
             new Image(Path.Combine("Assets", "Images", "blue-block.png")), false);
+
+        container = new EntityContainer<Block>();
 
         colHandler = new CollisionHandler();
     }
     private Player player;
     private Ball ball;
     private Block block;
+    private EntityContainer<Block> container;
     private CollisionHandler colHandler;
 
     [Test]
@@ -36,8 +39,40 @@ public class BallTest{
         Vec2F pos1 = ball.Shape.Position;
         ball.MoveBall();
         Vec2F pos2 = ball.Shape.Position;
-        float dist = System.MathF.Sqrt(
-            System.MathF.Pow((pos1.X + pos2.X), 2.0f) + System.MathF.Pow((pos1.Y + pos2.Y), 2.0f));
-        Console.WriteLine(dist);
+        float dist = System.MathF.Round(System.MathF.Sqrt(
+            System.MathF.Pow((pos2.X - pos1.X), 2.0f) + System.MathF.Pow((pos2.Y - pos1.Y), 2.0f)), 6);
+        Assert.That(dist, Is.EqualTo(System.MathF.Round(Ball.MOVEMENT_SPEED, 6)));
+    }
+
+    [Test]
+    public void TestUpdateDirectionX() {
+        ball.UpdateDirection(1.0f,2.0f);
+        Vec2F exRes = new Vec2F(1.0f,2.0f); 
+        Assert.That((ball.Shape.AsDynamicShape()).Direction.X, Is.EqualTo(exRes.X));
+    }
+
+    [Test]
+    public void TestUpdateDirectionY() {
+        ball.UpdateDirection(1.0f,2.0f);
+        Vec2F exRes = new Vec2F(1.0f,2.0f); 
+        Assert.That((ball.Shape.AsDynamicShape()).Direction.Y, Is.EqualTo(exRes.Y));
+    }
+
+    [Test]
+    public void TestBlockCollison() {
+        ball.UpdateDirection(0.01f,0.0f);
+        float dir = ball.Shape.AsDynamicShape().Direction.X;
+        container.AddEntity(block);
+        ball.MoveBall();
+        colHandler.BlockCollision(container, ball);
+        Assert.That(ball.Shape.AsDynamicShape().Direction.X, Is.EqualTo(-dir));
+    }
+
+    [Test]
+    public void TestPlayerCollison() {
+        ball.UpdateDirection(0.0f,-0.01f);
+        ball.MoveBall();
+        colHandler.PlayerCollision(player, ball);
+        Assert.IsTrue(ball.Shape.AsDynamicShape().Direction.Y > 0.0f);
     }
 }
