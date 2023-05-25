d60f742d212ab4ce6f243b96d40a158f8cd2310f
VTcommander <emil.vedel.thage@gmail.com>
2023-05-25 14:56:32 +0200

    sadas

---
+++ Breakout/Powers/Split.cs
@@ -0,0 +1,15 @@
+using Breakout;
+using DIKUArcade.Entities;
+
+namespace Breakout.Powers;
+
+public class Split : Entity {
+
+    public Split () : base(){
+        
+    }
+
+    public void Move(){}
+
+    public void Apply(){}
+}
