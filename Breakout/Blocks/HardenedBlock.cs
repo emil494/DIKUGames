49cd6242d6db069f5e45a1b6f79928d4ebc167d8
VTcommander <emil.vedel.thage@gmail.com>
2023-05-26 13:50:37 +0200

    LoseLife + ReduceTime hazards, refactor EffectG

--- Breakout/Blocks/HardenedBlock.cs
+++ Breakout/Blocks/HardenedBlock.cs
@@ -42,7 +42,23 @@ public class HardenedBlock : Entity, IBlock {
 
     public void DeleteBlock(){
         if (powerUp){
-            //To do: Create powerUp through EventBus
+            EventBus.GetBus().RegisterEvent(
+                new GameEvent {
+                    EventType = GameEventType.InputEvent, 
+                    Message = "ADD_POWERUP",
+                    StringArg1 = Shape.Position.X.ToString(),
+                    StringArg2 = Shape.Position.Y.ToString()
+                }
+            );
+        } else {
+            EventBus.GetBus().RegisterEvent(
+                new GameEvent {
+                    EventType = GameEventType.InputEvent, 
+                    Message = "ADD_HAZARD",
+                    StringArg1 = Shape.Position.X.ToString(),
+                    StringArg2 = Shape.Position.Y.ToString()
+                }
+            );
         }
         DeleteEntity();
         EventBus.GetBus().RegisterEvent(
