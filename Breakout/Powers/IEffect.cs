4fd3f7ab3d7c3684c2b6d894c6a865e442a5fc78
NikolajK1405 <nikolajkrarup@hotmail.com>
2023-06-03 14:40:11 +0000

    xml

--- Breakout/Powers/IEffect.cs
+++ Breakout/Powers/IEffect.cs
@@ -3,7 +3,18 @@ using Breakout;
 namespace Breakout.Powers;
 
 public interface IEffect{
+    /// <summary>
+    /// Moves the power up, and deletes it when it leaves the screen
+    /// </summary>
     void Move(){}
+    /// <summary>
+    /// Sends a message through the game bus, to the appropriate class, to apply the effect
+    /// </summary>
     void Apply(){}
+    /// <summary>
+    /// Checks for collision between the player and the powerup, 
+    /// and calls the Apply method if one occurs
+    /// </summary>
+    /// <param name="player">The player object, to check for collision with</param>
     void PlayerCollision(Player player){}
 }
