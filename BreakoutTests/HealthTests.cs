73c9ad134dbabda460f3ba514ecca06fded8ae24
VTcommander <emil.vedel.thage@gmail.com>
2023-06-05 12:25:29 +0200

    HealthTest

--- BreakoutTests/HealthTests.cs
+++ BreakoutTests/HealthTests.cs
@@ -1,9 +1,34 @@
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
-using Breakout.Blocks;
 using Breakout;
 using DIKUArcade.Events;
 
 namespace BreakoutTests;
 
+public class HealthTests {
+    private Health hp;
+    private BallHandler bh;
+
+    [SetUp]
+    public void Setup(){
+        EventBus.ResetBus();
+        hp = new Health();
+        bh = new BallHandler();
+        EventBus.GetBus().Subscribe(GameEventType.StatusEvent, hp);
+    }
+
+    [Test]
+    public void TestLoseHealth() {
+        bh.InitializeGame();
+        var player = new Player();
+        var container = new EntityContainer<Entity>();
+        var before = hp.GetHealth();
+        for (int i = 0; i <= 99; i++){
+            bh.UpdateBalls(container, player);
+        }
+        EventBus.GetBus().ProcessEvents();
+        var after = hp.GetHealth();
+        Assert.That(after, Is.LessThan(before));
+    }
+}
